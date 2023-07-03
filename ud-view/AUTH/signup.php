16583fa06f06cadc12c46a1937dae526067e1f16
= <=>
2023-07-03 06:43:38 -0400

    Simple Indentation

--- ud-view/AUTH/signup.php
+++ ud-view/AUTH/signup.php
@@ -18,7 +18,7 @@ Events::addListener('@auth//right', function () { ?>
                         if(empty(Uss::$global['options']->get("user:collect-username"))) {
                             return;
                         }
-                        ?>
+                ?>
 					<div class="mb-3">
 						<input type="text" placeholder="Username" class='form-control' name='username' required pattern="^\s*\w+\s*$">
 					</div>
@@ -26,71 +26,75 @@ Events::addListener('@auth//right', function () { ?>
 				
 				
 				<?php
-                            Events::addListener('@auth//form//signup', function () { ?>
-						<div class="mb-3">
-							<input type="email" placeholder="Email" class='form-control' name='email' required>
-						</div>
+                    Events::addListener('@auth//form//signup', function () { 
+				?>
+					<div class="mb-3">
+						<input type="email" placeholder="Email" class='form-control' name='email' required>
+					</div>
 				<?php }, EVENT_ID . 'email'); ?>
 				
 				
 				<?php
-                            Events::addListener('@auth//form//signup', function () { ?>
-						<!-- end col -->
-						<div class="mb-3">
-							<input type="password" placeholder="Password" class='form-control' name='password' required pattern='^.{4,}$'>
-						</div>
+                    Events::addListener('@auth//form//signup', function () { 
+				?>
+					<!-- end col -->
+					<div class="mb-3">
+						<input type="password" placeholder="Password" class='form-control' name='password' required pattern='^.{4,}$'>
+					</div>
 				<?php }, EVENT_ID . 'password'); ?>
 				
 				
 				<?php
-                            Events::addListener('@auth//form//signup', function () { ?>
-						<!-- end col -->
-						<div class="mb-4">
-							<input type="password" placeholder="Confirm Password" class='form-control' name='confirm_password' pattern='^.{4,}$' required>
-						</div>
+                    Events::addListener('@auth//form//signup', function () { 
+				?>
+					<!-- end col -->
+					<div class="mb-4">
+						<input type="password" placeholder="Confirm Password" class='form-control' name='confirm_password' pattern='^.{4,}$' required>
+					</div>
 				<?php }, EVENT_ID . 'password2'); ?>
 				
 				
 				<?php
-                            Events::addListener('@auth//form//signup', function () { ?>
-						<!-- end col -->
-						<div class='mb-4'>
-							<div class="form-check user-select-none">
-								<input class="form-check-input" type="checkbox" value="" id="tos" required>
-								<label class="form-check-label" for="tos">
-									<small>
-										I agree to the <a href='<?php echo Udash::config('tos-page') ?? 'javascript:void(0)'; ?>'>Terms Of Service</a> &amp; <a href='<?php echo Udash::config('privacy-page') ?? 'javascript:void(0)'; ?>'>Privacy Policy</a>
-									</small>
-								</label>
-							</div>
+                    Events::addListener('@auth//form//signup', function () { 
+				?>
+					<!-- end col -->
+					<div class='mb-4'>
+						<div class="form-check user-select-none">
+							<input class="form-check-input" type="checkbox" value="" id="tos" required>
+							<label class="form-check-label" for="tos">
+								<small>
+									I agree to the <a href='<?php echo Udash::config('tos-page') ?? 'javascript:void(0)'; ?>'>Terms Of Service</a> &amp; <a href='<?php echo Udash::config('privacy-page') ?? 'javascript:void(0)'; ?>'>Privacy Policy</a>
+								</small>
+							</label>
 						</div>
+					</div>
 				<?php }, EVENT_ID . 'policy'); ?>
 				
 				
 				<?php
-                            Events::addListener('@auth//form//signup', function () {
-                                if(empty(Uss::$global['options']->get('user:affiliation'))) {
-                                    return;
-                                }
-                                $parent = Udash::get_sponsor();
-                                if(!$parent) {
-                                    return;
-                                }
-                                ?>
+                    Events::addListener('@auth//form//signup', function () {
+						if(empty(Uss::$global['options']->get('user:affiliation'))) {
+							return;
+						}
+						$parent = Udash::get_sponsor();
+						if(!$parent) {
+							return;
+						}
+					?>
 						<input type='hidden' name='parent' value='<?php echo $parent['id']; ?>'>
 				<?php }, EVENT_ID . 'referral'); ?>
 				
 				
 				<?php
-                                    Events::addListener('@auth//form//signup', function () { ?>
-						<!-- end col -->
-						<div class="button-group d-flex justify-content-center flex-wrap">
-							<button class="btn btn-primary w-100" type='submit'>
-								Sign Up
-							</button>
-						</div>
-					<?php }, EVENT_ID . 'submit');
-    ?>
+                    Events::addListener('@auth//form//signup', function () { 
+				?>
+					<!-- end col -->
+					<div class="button-group d-flex justify-content-center flex-wrap">
+						<button class="btn btn-primary w-100" type='submit'>
+							Sign Up
+						</button>
+					</div>
+				<?php }, EVENT_ID . 'submit'); ?>
 				
 				
 				<?php Events::exec('@auth//form//signup'); ?>
