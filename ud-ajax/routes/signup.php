4095350d1a565264f50b702a3d5adecdb2893185
ucscode <uche23mail@gmail.com>
2023-07-07 11:16:09 -0400

    Changed Ajax Concept

--- ud-ajax/routes/signup.php
+++ ud-ajax/routes/signup.php
@@ -7,215 +7,200 @@ defined("UDASH_AJAX") or die;
 
 Events::addListener('udash:ajax', function () {
 
-    /**
-     * Define a redirect URL
-     * The URL that user will be redirected to after a successful registration
-     */
+    # Process The User Data;
 
-    $redirect = Udash::config('signup:redirect') ?? Core::url(ROOT_DIR . '/' . UDASH_ROUTE);
+    $result = call_user_func(function() {
 
+        # Trim all character
 
-    /**
-     * Trim all character
-     */
+        array_walk_recursive($_POST, function(&$value) {
+            $value = trim($value);
+        });
 
-    $_POST = array_map('trim', $_POST);
-
-
-    /**
-     * The table prefix
-     */
-    $prefix = DB_TABLE_PREFIX;
+        # The table prefix
+        
+        $prefix = DB_TABLE_PREFIX;
 
 
-    /**
-     * Check if the email address is valid
-     */
+        if( !preg_match(Core::regex('email'), $_POST['email']) ) {
+            
+            # Check if the email address is valid
 
-    if(!preg_match(Core::regex('email'), $_POST['email'])) {
-        $message = "The email address is not valid";
-    }
+            $message = "The email address is not valid";
 
+        } elseif( isset($_POST['username']) && !preg_match("/^\w+$/i", $_POST['username']) ) {
 
-    /**
-     * Check if username is valid
-     */
+            # Check if username is valid
 
-    elseif(isset($_POST['username']) && !preg_match("/^\w+$/i", $_POST['username'])) {
+            $message = "Username can only contain number, letter and underscore";
 
-        $message = "Username can only contain number, letter and underscore";
-    }
+        } elseif ($_POST['password'] !== $_POST['confirm_password']) {
 
+            # Check if password matches;
 
-    /**
-     * Check if password matches;
-     */
+            $message = "Password does not match";
 
-    elseif ($_POST['password'] !== $_POST['confirm_password']) {
-        $message = "Password does not match";
-    }
-
-
-    /**
-     * All conditions are met
-     */
-    else {
-
-        /**
-         * Check if the email address already exists;
-         */
-
-        $user = Udash::fetch_assoc("{$prefix}_users", $_POST['email'], 'email');
-
-        /**
-         * If a user if found
-         * Then, the email exists
-         */
-
-        if($user) {
-            $message = "The email already exists";
         } else {
 
-            /**
-             * Check if the username already exists;
-             */
+            # Check if the email address already exists;
+            
+            $user = Udash::fetch_assoc("{$prefix}_users", $_POST['email'], 'email');
 
-            $username = $_POST['username'] ?? null;
-            $user = Udash::fetch_assoc("{$prefix}_users", $username, 'username');
+            if($user) {
+                
+                # If a user if found, then, the email exists
+                
+                $message = "The email already exists";
 
-            /**
-             * If username is set &
-             * A user is found
-             * Then, the username exists
-             */
-            if(!empty($username) && $user) {
-                $message = "The username already exists";
             } else {
 
-                /*
-                    If parent ID is supplied:
-                    Then: confirm that the parent exists!
-                */
+                # Check if the username already exists;
 
-                if(isset($_POST['parent']) && !empty(Uss::$global['options']->get('user:affiliation'))) {
-                    $parent = Udash::fetch_assoc("{$prefix}_users", $_POST['parent']);
-                    if($parent) {
-                        $parent = $parent['id'];
-                    }
+                $username = $_POST['username'] ?? null;
+                $user = Udash::fetch_assoc("{$prefix}_users", $username, 'username');
+
+                /**
+                 * If username is set & A user is found
+                 * Then, the username exists
+                 */
+                if( !empty($username) && $user ) {
+
+                    $message = "The username already exists";
+
                 } else {
-                    $parent = null;
-                }
-
-                /**
-                 * Great!
-                 * All validation process has been confirmed!
-                 */
-
-                $data = array(
-                    "email" => $_POST['email'],
-                    "password" => Udash::password($_POST['password']),
-                    "username" => $username,
-                    "usercode" => Core::keygen(mt_rand(5, 7)),
-                    "parent" => $parent
-                );
-
-                /**
-                 * Insert into database
-                 * sQuery will auto sanitize the input
-                 */
-
-                $SQL = SQuery::insert("{$prefix}_users", $data, Uss::$global['mysqli']);
-
-                /**
-                 * Insert the user into database
-                 * Then get the userid immediately
-                 */
-                $status = Uss::$global['mysqli']->query($SQL);
-
-                $data['id'] = Uss::$global['mysqli']->insert_id;
-
-
-                /** On success */
-
-                if($status) {
 
                     /**
-                     * Assign a role to the new user!
+                     * If parent ID is supplied:
+                     * Then: confirm that the parent exists!
                      */
+                    if(isset($_POST['parent']) && !empty(Uss::$global['options']->get('user:affiliation'))) {
 
-                    $defaultRole = Uss::$global['options']->get('user:default-role');
+                        # Get the parent
 
-                    $assigned = Roles::user($data['id'])::assign($defaultRole);
+                        $parent = Udash::fetch_assoc("{$prefix}_users", $_POST['parent']);
+
+                        if($parent)  $parent = $parent['id'];
+                        
+                    } else $parent = null;
 
                     /**
-                     * Clear Access Token!
+                     * Great!
+                     * All validation process has been confirmed!
                      */
-                    Udash::setAccessToken(null);
+                    $data = array(
+                        "email" => $_POST['email'],
+                        "password" => Udash::password($_POST['password']),
+                        "username" => $username,
+                        "usercode" => Core::keygen(mt_rand(5, 7)),
+                        "parent" => $parent
+                    );
 
                     /**
-                     * The success message
+                     * Insert into database
+                     * sQuery will auto sanitize the input
                      */
-                    $message = "<i class='bi bi-check-circle text-success me-1'></i> Your registration was successful";
-
+                    $SQL = SQuery::insert("{$prefix}_users", $data, Uss::$global['mysqli']);
 
                     /**
-                     * Send verification email
-                    */
+                     * Insert the user into database
+                     * Then get the userid immediately
+                     */
+                    $status = Uss::$global['mysqli']->query($SQL);
 
-                    $verify_mail = !empty(Uss::$global['options']->get('user:confirm-email'));
+                    $data['id'] = Uss::$global['mysqli']->insert_id;
 
-                    if($verify_mail) {
 
-                        /**
-                         * Send a new confirmation link to the user
-                         * The user will be able to login on after verifying the link
-                         */
-                        $sent = Udash::send_confirmation_email($data['email']);
+                    # If status == true 
 
-                        $className = 'mt-3 pt-3 border-top fs-14px';
+                    if($status) {
 
-                        if($sent) {
+                        # ssign a role to the new user!
 
-                            $message .= "
-								<div class='{$className} text-success'> 
-									Please confirm the link sent to your email
-								</div>
-							";
+                        $defaultRole = Uss::$global['options']->get('user:default-role');
 
-                        } else {
+                        $assigned = Roles::user($data['id'])::assign($defaultRole);
 
-                            $message .= "
-								<div class='{$className} text-danger'> 
-									Email confirmation link failed to send. <br> Try requesting for a new link in the login form
-								</div>
-							";
+                        # Clear Access Token!
+                        
+                        Udash::setAccessToken(null);
+
+                        # The success message
+                        
+                        $message = "<i class='bi bi-check-circle text-success me-1'></i> Your registration was successful";
+                        
+
+                        # Send verification email
+
+                        $verify_mail = !empty(Uss::$global['options']->get('user:confirm-email'));
+
+                        if($verify_mail) {
+
+                            /**
+                             * Send a new confirmation link to the user
+                             * The user will be able to login on after verifying the link
+                             */
+                            $sent = Udash::send_confirmation_email($data['email']);
+
+                            $className = 'mt-3 pt-3 border-top fs-14px';
+
+                            if($sent) {
+
+                                $message .= "
+                                    <div class='{$className} text-success'> 
+                                        Please confirm the link sent to your email
+                                    </div>
+                                ";
+
+                            } else {
+
+                                $message .= "
+                                    <div class='{$className} text-danger'> 
+                                        Email confirmation link failed to send. <br> Try requesting for a new link in the login form
+                                    </div>
+                                ";
+
+                            };
 
                         };
 
-                    };
+                        # ------- [ End Email Verification ] -----------
 
-                    # ------- [ End Email Verification ] -----------
+                    } else {
 
-                } else {
+                        /**
+                         * If user detail was not inserted into database
+                         * Output a failure message
+                         */
+                        $message = "<i class='bi bi-x-circle text-danger me-1'></i> The registration was not successful";
 
-                    /**
-                     * If user detail was not inserted into database
-                     * Output a failure message
-                     */
-                    $message = "<i class='bi bi-x-circle text-danger me-1'></i> The registration was not successful";
+                    }
 
-                }
+                } // username doesn't exits
 
-            } // username doesn't exits
+            } // email doesn't exist
 
-        } // email doesn't exist
+        }; // all conditions are met
 
-    }; // all conditions are met
+
+        $redirect = Udash::config('signup:redirect') ?? Core::url(ROOT_DIR . '/' . UDASH_ROUTE);
+
+        return [
+            "status" => $status ?? false,
+            "message" => $message,
+            "redirect" => $redirect,
+            "user" => $data ?? null
+        ];
+
+    });
 
     /**
-     * Print the output and end the script
-    */
+     * Allow module processing;
+     * Hint: Values should be gotten by reference
+     */
+    Events::exec("udash:ajax/signup", $result ); 
+
+    # Print the output and end the script
     
-    Uss::stop($status ?? false, $message, array( 'redirect' => $redirect ));
+    Uss::stop( $result['status'], $result['message'], $result['redirect'] );
     
 }, 'ajax-signup');
