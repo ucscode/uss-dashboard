0891b8f6e1f63d683fdad4bb2a59f28fbda1f2cb
ucscode <uche23mail@gmail.com>
2023-10-25 11:06:09 +0100

    v3.0.0 dev | feat(Extension Update)

--- bundles/class/DashboardTwigExtension.php
+++ bundles/class/DashboardTwigExtension.php
@@ -6,41 +6,58 @@ final class DashboardTwigExtension extends \Twig\Extension\AbstractExtension imp
 {
     public readonly TreeNode $menu;
     public readonly TreeNode $userMenu;
+    public readonly string $themeName;
+    public readonly string $themePath;
+    public readonly string $themeNamespace;
+    public readonly string $themeUrl;
 
     public function getGlobals(): array
     {
-        return ['Ud' => $this];
+        return ['DashboardExtension' => $this];
     }
 
     public function __construct(
         private AbstractDashboard $dashboard
     ) {
-        $this->menu = $this->dashboard->menu;
-        $this->userMenu = $this->dashboard->userMenu;
+        $this->setInitialValues();
         Alert::flushAll();
     }
 
-    public function getDashboardProperty(string $property): mixed
-    {
-        return $this->dashboard->{$property} ?? null;
-    }
-
     /**
-     * Uss Methods
+     * @method getAttribute
      */
     public function getAttribute(string $property)
     {
         return $this->dashboard->getAttribute($property);
     }
 
+    /**
+     * @method urlGenerator
+     */
     public function urlGenerator(string $path = '', array $param = []): string
     {
         return $this->dashboard->urlGenerator($path, $param)->getResult();
     }
 
+    /**
+     * @method getArchiveUrl
+     */
     public function getArchiveUrl(string $pagename): ?string
     {
         return $this->dashboard->getArchiveUrl($pagename);
     }
 
+    /**
+     * @method setInitialValues
+     */
+    public function setInitialValues(): void
+    {
+        $uss = Uss::instance();
+        $this->menu = $this->dashboard->menu;
+        $this->userMenu = $this->dashboard->userMenu;
+        $this->themeName = $this->dashboard->config->theme;
+        $this->themePath = DashboardImmutable::THEME_DIR . '/' . $this->themeName;
+        $this->themeNamespace = '@Theme/' . $this->themeName;
+        $this->themeUrl = $uss->abspathToUrl($this->themePath);
+    }
 }
