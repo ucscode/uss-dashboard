14fa015b4f2ee60f7a2caf71d83e2ec0e3b3b569
ucscode <uche23mail@gmail.com>
2024-01-12 03:50:15 +0100

    v5.7.15 | feat: Notification page with new design

--- Foundation/User/Controller/NotificationController.php
+++ Foundation/User/Controller/NotificationController.php
@@ -2,9 +2,20 @@
 
 namespace Module\Dashboard\Foundation\User\Controller;
 
+use Module\Dashboard\Bundle\Common\Document;
 use Module\Dashboard\Bundle\Kernel\Abstract\AbstractDashboardController;
-use Uss\Component\Route\RouteInterface;
+use Module\Dashboard\Bundle\Kernel\Interface\DashboardInterface;
+use Module\Dashboard\Bundle\Kernel\Interface\DashboardFormInterface;
+use Module\Dashboard\Bundle\User\User;
 
 class NotificationController extends AbstractDashboardController
 {
-};
+    public function composeApplication(DashboardInterface $dashboard, Document $document, ?DashboardFormInterface $form): void
+    {
+        $user = (new User())->acquireFromSession();
+        $notifications = $user->notification->get(['hidden' => 0,]);
+        $document->setContext([
+            'notifications' => $notifications,
+        ]);
+    }
+}
