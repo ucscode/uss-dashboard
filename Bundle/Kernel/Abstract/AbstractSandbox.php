0e320fa5e64d04fa2155c092a7eae67b1237fbf8
ucscode <uche23mail@gmail.com>
2024-02-20 08:48:04 +0100

    feat: Sandbox inheritance of filesystem path from global Uss

--- Bundle/Kernel/Abstract/AbstractSandbox.php
+++ Bundle/Kernel/Abstract/AbstractSandbox.php
@@ -2,7 +2,6 @@
 
 namespace Module\Dashboard\Bundle\Kernel\Abstract;
 
-use Module\Dashboard\Bundle\Kernel\Compact\DashboardEnvironment;
 use Uss\Component\Kernel\Abstract\AbstractUss;
 use Uss\Component\Kernel\Extension\Extension;
 use Uss\Component\Kernel\Uss;
@@ -15,7 +14,7 @@ abstract class AbstractSandbox extends AbstractUss
     public function __construct()
     {
         parent::__construct();
-        new DashboardEnvironment($this);
+        $this->inheritUssFilesystemPaths();
         $this->borrowedExtension = new Extension(Uss::instance());
         $this->twig->addExtension($this->borrowedExtension);
         $this->isLocalhost = in_array($_SERVER['SERVER_NAME'], ['localhost', '127.0.0.1', '::1'], true);
@@ -26,5 +25,14 @@ abstract class AbstractSandbox extends AbstractUss
         $this->borrowedExtension->configureRenderContext();
         $context += Uss::instance()->templateContext;
         return $this->twig->render($template, $context);
+    }
+
+    private function inheritUssFilesystemPaths(): void
+    {
+        $ussLoader = Uss::instance()->filesystemLoader;
+        $danglingNamespaces = array_diff($ussLoader->getNamespaces(), $this->filesystemLoader->getNamespaces());
+        foreach($danglingNamespaces as $namespace) {
+            $this->filesystemLoader->setPaths($ussLoader->getPaths($namespace), $namespace);
+        }
     }
 }
