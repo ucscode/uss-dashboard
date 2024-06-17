98278c4e13dc6298fcb1bb3dcd0f9fad03d82f6c
ucscode <uche23mail@gmail.com>
2024-06-17 20:32:04 +0100

    fix: Logout on user update

--- Foundation/Admin/Controller/Users/Process/OnUpdateSubmit.php
+++ Foundation/Admin/Controller/Users/Process/OnUpdateSubmit.php
@@ -4,6 +4,7 @@ namespace Module\Dashboard\Foundation\Admin\Controller\Users\Process;
 
 use Module\Dashboard\Bundle\Kernel\Abstract\AbstractDashboardForm;
 use Module\Dashboard\Bundle\User\User;
+use Uss\Component\Kernel\Uss;
 
 class OnUpdateSubmit extends AbstractUserFormSubmit
 {
@@ -20,11 +21,20 @@ class OnUpdateSubmit extends AbstractUserFormSubmit
         }
     }
 
+    /**
+     * @param \Uss\Component\Common\Entity $response
+     */
     public function onPersist(mixed &$response, AbstractDashboardForm $form): void
     {
         if(!empty($this->postContext['password']) && $response) {
-            $this->client = (new User($response['id']))->saveToSession();
+            $this->client = new User($response['id']);
+            $currentUser = (new User())->acquireFromSession();
+            
+            if($this->client->getId() === $currentUser->getId()) {
+                $this->client->saveToSession();
+            }
         }
+        
         parent::onPersist($response, $form);
     }
 }
